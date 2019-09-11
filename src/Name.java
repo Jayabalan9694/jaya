93accbcbe044b50b81dec70829b6c76b662c6784
Jayabalan9694 <jayabal9694@gmail.com>
2019-09-11 13:18:52 +0530

    java added

---
+++ src/Name.java
@@ -0,0 +1,16 @@
+
+public class Name {
+public static void main(String[] args) {
+	String s="gfcgfcf";
+	String s2="gg";
+	String merge = s2.concat(s);
+	System.out.println(merge);
+	String str="GreensTechnology";
+	int len = str.length();
+	System.out.println(len);
+	String sw="SeleniumAutomationtool";
+	int le = sw.length();
+	System.out.println(leb);
+	
+}
+}
